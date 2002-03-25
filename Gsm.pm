cb633cf85538eab062cd53c66763b38bf778b8dc
Cosimo Streppone <cosimo@cpan.org>
2002-03-25 06:24:06 +0000

    started source cleanup + licence
    git-svn-id: svn://localhost:81/cpan/perl5-device-gsm/trunk@6 16b0436d-b983-6540-bd0b-04945bd77856

--- Gsm.pm
+++ Gsm.pm
@@ -1,26 +1,45 @@
-# Declare object name
-package device::gsm;
-$device::gsm::VERSION = sprintf("%d.%02d", q$Revision: 1.1.1.1 $ =~ /(\d+)\.(\d+)/);
+# Device::Gsm - a Perl class to interface GSM devices as AT modems
+# Copyright (C) 2000-2002 Cosimo Streppone, cosimo@cpan.org
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# WARNING
+#
+# This is PRE-ALPHA software, still needs extensive testing and
+# support for custom GSM commads, so use it at your own risk,
+# and without ANY warranty! Have fun.
+#
+# $Id: Gsm.pm,v 1.2 2002-03-25 06:24:06 cosimo Exp $
 
-# Module dependencies
+package Device::Gsm;
+$Device::Gsm::VERSION = sprintf "%d.%02d", q$Revision: 1.2 $ =~ /(\d+)\.(\d+)/);
+
 use strict;
-use init;
-use log;
-
-# This is an AT-compliant device
-use device::at;
+use Device::SerialPort;
+use Device::Modem;
 
 # Connection defaults
-$device::gsm::BAUDRATE = 9600;
+$Device::Gsm::BAUDRATE = 9600;
 
 # Hierarchy information
-@device::gsm::ISA = qw( device::at );
+@Device::Gsm::ISA = qw( Device::Modem );
 
 #/**
 # * @method       connect
 # *
 # * Connect on serial port to gsm device
-# * via serial port
 # *
 # * @param        reference to hash of options, that must contain:
 # *     BAUDRATE  speed of communication (default 9600)
@@ -34,7 +53,7 @@ sub connect {
 	my ($me,$rOpt) = @_;
 
 	# GSM defaults to 9600 baud
-	$rOpt->{BAUDRATE} ||= $device::gsm::BAUDRATE;
+	$rOpt->{BAUDRATE} ||= $Device::Gsm::BAUDRATE;
 
 	$me->SUPER::connect( $rOpt );
 }
@@ -61,7 +80,7 @@ sub register {
 
 	# Send PIN status query
 	$me->log->write( 'PIN status query' );
-	$me->atsend( 'AT+CPIN?' . device::at::CR );
+	$me->atsend( 'AT+CPIN?' . Device::Modem::CR );
 	
 	# Get answer
 	my $cReply = $me->answer();
@@ -75,7 +94,7 @@ sub register {
 		
 		# Pin request, sending PIN code
 		$me->log->write( 'PIN requested: sending...' );
-		$me->atsend( qq[AT+CPIN="$$me{'PIN'}"] . device::at::CR );
+		$me->atsend( qq[AT+CPIN="$$me{'PIN'}"] . Device::Modem::CR );
 		
 		# Get reply
 		$cReply = $me->answer();
@@ -100,7 +119,7 @@ sub register {
 }
 
 #/**
-# * @method       sendSms
+# * @method       send_sms
 # *
 # * Send SMS to handphone number [cRecipient]. The text of
 # * SMS to send is in [cText]. Returns a boolean value telling
@@ -116,7 +135,7 @@ sub register {
 # * @return       lSuccess
 # * 	success of sending
 # */
-sub sendSms {
+sub send_sms {
 	my($me, $num, $text) = @_;
 	my $lOk = 0;
 	my $cReply;
@@ -136,8 +155,8 @@ sub sendSms {
 	} else {
 		
 		# Send sms
-		$me->atsend( qq[AT+CMGS="$num"] . device::at::CR );
-		$me->atsend( $text . device::at::CTRL_Z );
+		$me->atsend( qq[AT+CMGS="$num"] . Device::Modem::CR );
+		$me->atsend( $text . Device::Modem::CTRL_Z );
 		
 		# Get reply and check for errors
 		$cReply = $me->answer();
