b45e029748288018b60ed236b0a1cac792b13c68
Cosimo Streppone <cosimo@cpan.org>
2003-03-24 23:06:09 +0000

    moved Gsm.html documentation
    git-svn-id: svn://localhost:81/cpan/perl5-device-gsm/trunk@100 16b0436d-b983-6540-bd0b-04945bd77856

--- makedoc.sh
+++ makedoc.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
-# $Id: makedoc.sh,v 1.2 2003-03-24 23:03:45 cosimo Exp $
+# $Id: makedoc.sh,v 1.3 2003-03-24 23:06:09 cosimo Exp $
 #
 
 #mv README README.old
 #pod2text Gsm.pm >README
 
-mv Gsm.html Gsm.html.old
-pod2html Gsm.pm >Gsm.html
+mv docs/Gsm.html docs/Gsm.html.old
+pod2html Gsm.pm >docs/Gsm.html
 
-perl -i.bak -pe 's/dev\.xproject\.org/cpan.org/g' Gsm.html
+perl -i.bak -pe 's/dev\.xproject\.org/cpan.org/g' docs/Gsm.html
 
