8fc4ba8a247bfeb0013b928c512f09bc4c1b576d
Cosimo Streppone <cosimo@cpan.org>
2003-09-14 09:42:10 +0000

    fixed error in reading default serial port and pin number
    git-svn-id: svn://localhost:81/cpan/perl5-device-gsm/trunk@109 16b0436d-b983-6540-bd0b-04945bd77856

--- examples/read_messages.pl
+++ examples/read_messages.pl
@@ -3,14 +3,14 @@
 # Short example of use for Device::Gsm class
 # Script that reads all SMS stored on SIM
 #
-# $Id: read_messages.pl,v 1.1 2003-03-23 14:39:31 cosimo Exp $
+# $Id: read_messages.pl,v 1.2 2003-09-14 09:42:10 cosimo Exp $
 
 use strict;
 use lib '../lib';
 use lib '../';
 use Gsm;
 
-print "\nthis is ", '$Id: read_messages.pl,v 1.1 2003-03-23 14:39:31 cosimo Exp $', "\n";
+print "\nthis is ", '$Id: read_messages.pl,v 1.2 2003-09-14 09:42:10 cosimo Exp $', "\n";
 print "\nTrying to read all messages you have on your SIM card...\n";
 
 my $port = $ENV{'DEV_GSM_PORT'} || ( $^O =~ /Win/ ? 'COM2' : '/dev/ttyS1' );
@@ -22,16 +22,16 @@ my $mypin;
 unless( $port ) {
 	print "Select your serial port [$port] : ";
 	chomp( $myport = <STDIN> );
-	$myport ||= $port;
 }
+$myport ||= $port;
 
 unless( $pin ) {
 	print "Insert your PIN number if you need to register to GSM network [$pin] : ";
 	chomp( $mypin = <STDIN> );
-	$mypin ||= $pin;
 	$mypin =~ s/\D//g;
 	$mypin = substr( $mypin, 0, 4 );
 }
+$mypin ||= $pin;
 
 my $gsm = new Device::Gsm(
 	port => $myport,
@@ -41,11 +41,18 @@ my $gsm = new Device::Gsm(
 
 die "cannot create Device::Gsm object!" unless $gsm;
 
+print "Connecting on $myport port...";
+
 $gsm->connect( baudrate => 19200 ) or die "cannot connect to GSM device on [$myport]\n";
+
+print " ok\n";
+print "Registering on GSM network...";
+
 $gsm->register() or die "cannot register on GSM network: check pin and/or network signal!"; 
 
-print "\nok! connected and registered to network.\n";
+print " ok\n";
 
+print "Connected and registered to network.\n";
 
 my @msg = $gsm->messages();
 my $lOk = scalar @msg;
