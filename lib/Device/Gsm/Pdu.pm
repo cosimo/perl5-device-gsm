846c94d8acc4fb69e08312a0be26e59d37e8095e
Cosimo Streppone <cosimo@opera.com>
2012-03-08 10:12:38 +0100

    Applied my current perltidy style

--- lib/Device/Gsm/Pdu.pm
+++ lib/Device/Gsm/Pdu.pm
@@ -1,4 +1,4 @@
-# Device::Gsm::Pdu - PDU encoding/decoding functions for Device::Gsm class 
+# Device::Gsm::Pdu - PDU encoding/decoding functions for Device::Gsm class
 # Copyright (C) 2002-2011 Cosimo Streppone, cosimo@cpan.org
 # Copyright (C) 2006-2011 Grzegorz Wozniak, wozniakg@gmail.com
 #
@@ -23,12 +23,12 @@ use strict;
 use Device::Gsm::Charset;
 use Device::Gsm::Sms::Token::UDH;
 
-# decode a pdu encoded phone number into human readable format 
+# decode a pdu encoded phone number into human readable format
 sub decode_address {
     my $address = shift or return;
 
     my $number;
-    my($length, $type, $bcd_digits) = unpack('A2 A2 A*', $address);
+    my ($length, $type, $bcd_digits) = unpack('A2 A2 A*', $address);
 
     # XXX DEBUG
     #print STDERR "len=$length type=$type bcd=$bcd_digits\n";
@@ -37,15 +37,14 @@ sub decode_address {
     # Alphabetical addresses begin with 'D0'.
     # Check also http://smslink.sourceforge.net/pdu.html
     #
-    if( $type eq 'D0' )
-    {
+    if ($type eq 'D0') {
         $number = decode_text7($length . $bcd_digits);
         return $number;
     }
 
     # Reverse each pair of bcd digits
-    while( $bcd_digits ) {
-        $number .= reverse substr( $bcd_digits, 0, 2 );
+    while ($bcd_digits) {
+        $number .= reverse substr($bcd_digits, 0, 2);
         $bcd_digits = substr $bcd_digits, 2;
     }
 
@@ -60,8 +59,7 @@ sub decode_address {
     $number =~ s/B/#/;
 
     # If number is international, put a '+' sign before
-    if( $type == 91 && $number !~ /^\s*\+/ )
-    {
+    if ($type == 91 && $number !~ /^\s*\+/) {
         $number = '+' . $number;
     }
 
@@ -70,35 +68,39 @@ sub decode_address {
 
 sub decode_text7 {
     pack '(b*)*',
-    unpack 'C/(a7)',
-    pack 'C a*',
-    unpack 'C b*',
-    pack 'H*', $_[0]
+        unpack 'C/(a7)',
+        pack 'C a*',
+        unpack 'C b*',
+        pack 'H*', $_[0];
 }
+
 #remains for comatibility reasons with my production scripts :)
 sub decode_text7_udh1 {
-    my $unpacked=join '',
-    unpack 'C/(a7)',
-    pack 'C a*',
-    unpack 'C b*',
-    pack 'H*', $_[0];
+    my $unpacked = join '',
+        unpack 'C/(a7)',
+        pack 'C a*',
+        unpack 'C b*',
+        pack 'H*', $_[0];
+
     #remove bit of padding from message
-    $unpacked=substr($unpacked,1,length($unpacked));
-    pack '(b*)*',($unpacked =~ m/([01]{1,7})/gs);
+    $unpacked = substr($unpacked, 1, length($unpacked));
+    pack '(b*)*', ($unpacked =~ m/([01]{1,7})/gs);
 }
+
 #decode text with padding
 sub decode_text7_udh {
-    my ($encoded,$padding) =@_;
-    $padding =0 unless($padding);
-    my $unpacked=join '',
-    unpack 'C/(a7)',
-    pack 'C a*',
-    unpack 'C b*',
-    pack 'H*', $encoded;
+    my ($encoded, $padding) = @_;
+    $padding = 0 unless ($padding);
+    my $unpacked = join '',
+        unpack 'C/(a7)',
+        pack 'C a*',
+        unpack 'C b*',
+        pack 'H*', $encoded;
+
     #remove bits of padding from message
-    $unpacked=substr($unpacked,$padding,length($unpacked));
-    pack '(b*)*',($unpacked =~ m/([01]{7})/gs);
-    
+    $unpacked = substr($unpacked, $padding, length($unpacked));
+    pack '(b*)*', ($unpacked =~ m/([01]{7})/gs);
+
 }
 
 # decode 8-bit encoded text
@@ -108,11 +110,12 @@ sub decode_text8($) {
     return unless $text8;
 
     my $str;
-    while( $text8 ) {
-        $str .= chr( hex(substr $text8, 0, 2) );
-        if( length($text8) > 2 ) {
+    while ($text8) {
+        $str .= chr(hex(substr $text8, 0, 2));
+        if (length($text8) > 2) {
             $text8 = substr($text8, 2);
-        } else {
+        }
+        else {
             $text8 = '';
         }
     }
@@ -120,9 +123,9 @@ sub decode_text8($) {
 }
 
 sub encode_address {
-    my $num  = shift;
-    my $type = '';
-    my $len  = 0;
+    my $num     = shift;
+    my $type    = '';
+    my $len     = 0;
     my $encoded = '';
 
     $num =~ s/\s+//g;
@@ -130,27 +133,26 @@ sub encode_address {
     #warn('encode_address('.$num.')');
 
     # Check for alphabetical addresses (TS 03.38)
-    if( $num =~ /[A-Z][a-z]/ )
-    {
+    if ($num =~ /[A-Z][a-z]/) {
+
         # Encode clear text in gsm0338 7-bit
-        $type = 'D0';
+        $type    = 'D0';
         $encoded = encode_text7($num);
-        $len  = unpack 'H2' => chr( length $encoded );
+        $len     = unpack 'H2' => chr(length $encoded);
     }
-    else
-    {
-        $type = index($num,'+') == 0 ? 91 : 81;
+    else {
+        $type = index($num, '+') == 0 ? 91 : 81;
 
         # Remove all non-numbers. Beware to GPRS dialing chars.
         $num =~ s/[^\d\*#]//g;
-        $num =~ s/\*/A/g;         # "*" maps to A
-        $num =~ s/#/B/g;          # "#" maps to B
+        $num =~ s/\*/A/g;        # "*" maps to A
+        $num =~ s/#/B/g;         # "#" maps to B
 
-        $len  = unpack 'H2' => chr( length $num );
+        $len = unpack 'H2' => chr(length $num);
         $num .= 'F';
         my @digit = split // => $num;
 
-        while( @digit > 1 ) {
+        while (@digit > 1) {
             $encoded .= join '', reverse splice @digit, 0, 2;
         }
     }
@@ -161,90 +163,108 @@ sub encode_address {
 }
 
 sub decode_text_UCS2 {
-    my $encoded= shift;
+    my $encoded = shift;
     return undef unless $encoded;
-    
-    my $len = hex substr( $encoded, 0, 2 );
+
+    my $len = hex substr($encoded, 0, 2);
     $encoded = substr $encoded, 2;
-    
+
     my $decoded = "";
     while ($encoded) {
-        $decoded .= pack("C0U",hex(substr($encoded,0,4)));
-        $encoded = substr($encoded, 4);     
+        $decoded .= pack("C0U", hex(substr($encoded, 0, 4)));
+        $encoded = substr($encoded, 4);
     }
     return $decoded;
 }
 
 sub encode_text7 {
     uc
-    unpack 'H*',
-    pack 'C b*',
-    length $_[0],
-    join '',
-    unpack '(b7)*', $_[0];
+        unpack 'H*',
+        pack 'C b*',
+        length $_[0],
+        join '',
+        unpack '(b7)*', $_[0];
 }
+
 #
 #return complete ud with udh
 #remains for comatibility reasons with my production scripts :)
 #
 sub encode_text7_udh1 {
-    my $decoded= shift;
-    my $udh1=shift;
-    my $decoded_length=length($decoded);
-    $decoded=Device::Gsm::Charset::iso8859_to_gsm0338($decoded);
-    my $pdu_msg=uc  
-    unpack 'H*',
-    pack 'b*',
-    #add one bit of padding to align septet boundary 
-    '0' .join '',
-    unpack '(b7)*', $decoded;
+    my $decoded        = shift;
+    my $udh1           = shift;
+    my $decoded_length = length($decoded);
+    $decoded = Device::Gsm::Charset::iso8859_to_gsm0338($decoded);
+    my $pdu_msg = uc
+        unpack 'H*',
+        pack 'b*',
+
+        #add one bit of padding to align septet boundary
+        '0' . join '', unpack '(b7)*', $decoded;
+
     #below add 7 septets length for udh1
-    return sprintf("%02X",$decoded_length + Sms::Token::UDH::UDH1_LENGTH) . $udh1 . $pdu_msg;
+    return
+          sprintf("%02X", $decoded_length + Sms::Token::UDH::UDH1_LENGTH)
+        . $udh1
+        . $pdu_msg;
 }
+
 #
 #encode text with padding
 #
 sub encode_text7_udh {
-    my $decoded= shift;
-    my $padding=shift;
-    $padding =0 unless($padding);
-    my $decoded_length=length($decoded);
-    $decoded=Device::Gsm::Charset::iso8859_to_gsm0338($decoded);
-    my $pdu_msg=uc  
-    unpack 'H*',
-    pack 'b*',
-    #add bits of padding to align septet boundary 
-    '0' x $padding .join '',
-    unpack '(b7)*', $decoded;
+    my $decoded = shift;
+    my $padding = shift;
+    $padding = 0 unless ($padding);
+    my $decoded_length = length($decoded);
+    $decoded = Device::Gsm::Charset::iso8859_to_gsm0338($decoded);
+    my $pdu_msg = uc
+        unpack 'H*',
+        pack 'b*',
+
+        #add bits of padding to align septet boundary
+        '0' x $padding . join '', unpack '(b7)*', $decoded;
+
     #below add septets length of text
-    my $len_hex=sprintf("%02X",$decoded_length);    
-    return wantarray?($len_hex,$pdu_msg,$len_hex.$pdu_msg):$len_hex.$pdu_msg;
+    my $len_hex = sprintf("%02X", $decoded_length);
+    return
+        wantarray
+        ? ($len_hex, $pdu_msg, $len_hex . $pdu_msg)
+        : $len_hex . $pdu_msg;
 }
 
 sub pdu_to_latin1 {
-	# Reattach a length octet.
-	my $s = shift;
-	my $len = length $s;
-	#arn "len=$len, len/2=", $len/2, "\n";
-	my $l = uc unpack("H*", pack("C", int(length($s)/2*8/7)));
-	if (length($l) % 2 == 1) { $l = '0'.$l }
-	my $pdu = $l . $s;
-	#arn "l=$l, pdu=$pdu\n";
-	my $decoded = Device::Gsm::Pdu::decode_text7($pdu);
-	#arn "decoded_text7=$decoded\n";
-	my $latin1 = Device::Gsm::Charset::gsm0338_to_iso8859($decoded);
-	#arn "latin1=$latin1\n";
-	return $latin1;
+
+    # Reattach a length octet.
+    my $s   = shift;
+    my $len = length $s;
+
+    #arn "len=$len, len/2=", $len/2, "\n";
+    my $l = uc unpack("H*", pack("C", int(length($s) / 2 * 8 / 7)));
+    if (length($l) % 2 == 1) { $l = '0' . $l }
+    my $pdu = $l . $s;
+
+    #arn "l=$l, pdu=$pdu\n";
+    my $decoded = Device::Gsm::Pdu::decode_text7($pdu);
+
+    #arn "decoded_text7=$decoded\n";
+    my $latin1 = Device::Gsm::Charset::gsm0338_to_iso8859($decoded);
+
+    #arn "latin1=$latin1\n";
+    return $latin1;
 }
 
 sub latin1_to_pdu {
-	my $latin1_text = $_[0];
-	#arn "latin1=$latin1_text\n";
-	my $gsm0338 = Device::Gsm::Charset::iso8859_to_gsm0338($latin1_text);
-	#arn "gsm0338=$gsm0338\n";
-	my $fullpdu = Device::Gsm::Pdu::encode_text7($gsm0338);
-	#arn "pdu=$fullpdu\n";
-	return substr($fullpdu, 2); # strip off the length octet
+    my $latin1_text = $_[0];
+
+    #arn "latin1=$latin1_text\n";
+    my $gsm0338 = Device::Gsm::Charset::iso8859_to_gsm0338($latin1_text);
+
+    #arn "gsm0338=$gsm0338\n";
+    my $fullpdu = Device::Gsm::Pdu::encode_text7($gsm0338);
+
+    #arn "pdu=$fullpdu\n";
+    return substr($fullpdu, 2);    # strip off the length octet
 }
 
 1;
